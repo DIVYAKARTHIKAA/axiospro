5134db902e95964b8e88887a57b3ab643cec5531
DIVYAKARTHIKAA <727821tucs046@skct.edu.in>
2022-12-12 19:10:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -21,5 +21,37 @@ function App() {
     </div>
   );
 }
+
+export default App;*/
+
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from './functions';
+
+class App extends Component {
+
+render() {
+
+        return (
+   
+      <div
+        <button
+aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
++
+        </button>
+        <span className={styles.value}>{count}</span>
+        <button
+aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+      </div>
+     
+  )
+}
+};*/
 
 export default App;
